dee32189a3e56ba1253ff711343ab5c02772d815
sundaram tiwari <sundaram.tiwari@nobroker.in>
2016-07-13 16:21:35 +0530

    added chat bot code

--- index.js
+++ index.js
@@ -9,6 +9,37 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+app.get('/webhook/', function (request, response) {
+	if (request.query['hub.verify_token'] === 'Nobroker_Labs') {
+		response.send(request.query['hub.challenge'])
+	}
+	response.send('Error, wrong token')
+})
+
+// API endpoint
+
+app.post('/webhook', function (request, response){
+	messaging_events = request.body.entry[0].messaging
+	for (i=0; i < messaging_events.length; i++) {
+		event = request.body.entry[0].messaging[i]
+		sender = event.sender.id
+		if (event.message && event.message.text) {
+			text = event.message.text
+			if (text === 'Aha') {
+				sendGenericMessage(sender)
+				continue
+			}
+			sendTextMessage(sender, "Yo: " + text.substring(0, 200))
+		}
+		if (event.postback) {
+			text = JSON.stringify(event.postback)
+			sendTextMessage(sender, "Postback received: " + text.substring(0, 200), token)
+			continue
+		}
+	}
+	response.sendStatus(200)
+})
+
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
