8ed6f26e8bdd451ec04bde1fc5a9d908d9c40f3b
sundaram tiwari <sundaram.tiwari@nobroker.in>
2016-07-15 17:55:01 +0530

    Added app config: express.bodyParser().

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
+app.configure(function(){
+  app.use(express.bodyParser());
+  app.use(app.router);
+});
+
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -192,5 +197,3 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
