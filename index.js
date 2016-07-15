15149253e610bbf42224681e068f16cce318353e
sundaram tiwari <sundaram.tiwari@nobroker.in>
2016-07-15 18:15:44 +0530

    commented receivedDeliveryConfirmation line 47

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.post('/webhook', function (request, response) {
         } else if (messagingEvent.message) {
           receivedMessage(messagingEvent);
         } else if (messagingEvent.delivery) {
-          receivedDeliveryConfirmation(messagingEvent);
+          // receivedDeliveryConfirmation(messagingEvent);
         } else if (messagingEvent.postback) {
           receivedPostback(messagingEvent);
         } else {
@@ -191,7 +191,22 @@ function sendGenericMessage(recipientId) {
   callSendAPI(messageData);
 }
 
+function receivedPostback(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfPostback = event.timestamp;
 
+  // The 'payload' param is a developer-defined field which is set in a postback 
+  // button for Structured Messages. 
+  var payload = event.postback.payload;
+
+  console.log("Received postback for user %d and page %d with payload '%s' " + 
+    "at %d", senderID, recipientID, payload, timeOfPostback);
+
+  // When a postback is called, we'll send a message back to the sender to 
+  // let them know it was successful
+  sendTextMessage(senderID, "Postback called");
+}
 
 app.get('/', function(request, response) {
   response.render('pages/index');
